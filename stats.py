fc406f1c506cfb18f8cde41d60992eb9cc9ebb30
Bedi <bednar1230@gmail.com>
2023-01-06 02:00:49 +0100

    roc

--- stats.py
+++ stats.py
@@ -3,6 +3,9 @@ from model import KERAS_MODEL_FILE_PATH, BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH
 from countries import get_countries
 from models.report.confusion_matrix import confusion_matrix as cm
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 
 
 def cross_validation(model) -> float:
@@ -62,6 +65,42 @@ def get_stats_from_confusion_matrix(confusion_matrix):
     }
 
 
+def plot_roc_for_given_class(model, class_name):
+    labels = np.array([])
+    predictions = np.array([])
+    for seed in range(50):
+        val_ds: tf.data.Dataset = tf.keras.utils.image_dataset_from_directory(
+            'flags',
+            validation_split=0.2,
+            seed=seed,
+            subset='validation',
+            batch_size=BATCH_SIZE,
+            image_size=(IMG_HEIGHT, IMG_WIDTH)
+        )
+
+        for x, y in val_ds:
+            labels = np.concatenate([labels, y])
+            predictions = np.concatenate([predictions, np.argmax(model.predict(x), axis=-1)])
+
+    class_i = sorted(get_countries()).index(class_name)
+    binarize = np.vectorize(lambda e: 0 if e == class_i else 1)
+    labels = binarize(labels)
+    predictions = binarize(predictions)
+
+    roc_auc = roc_auc_score(labels, predictions)
+    fpr, tpr, thresholds = roc_curve(labels, predictions)
+
+    plt.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc:0.2f})')
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'Receiver Operating Characteristic: {class_name}')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
 if __name__ == '__main__':
     model = tf.keras.models.load_model(KERAS_MODEL_FILE_PATH)
 
@@ -76,3 +115,5 @@ if __name__ == '__main__':
         file.write(stats_from_confusion_matrix)
 
     print(stats_from_confusion_matrix)
+
+
