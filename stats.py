d444fd1eabd2a2d867c1d0f4838b04ef42a60e7d
Bedi <bednar1230@gmail.com>
2023-01-06 01:10:26 +0100

    Stats better

--- stats.py
+++ stats.py
@@ -44,41 +44,21 @@ def get_confusion_matrix(model):
 
 
 def get_stats_from_confusion_matrix(confusion_matrix):
-    countries = sorted(get_countries())
+    total_samples = tf.reduce_sum(confusion_matrix)
+    true_positives = tf.linalg.trace(confusion_matrix)
+    row_sums = tf.reduce_sum(confusion_matrix, axis=1)
+    col_sums = tf.reduce_sum(confusion_matrix, axis=0)
 
-    numpy_confusion_matrix = np.array(confusion_matrix)
-    diagonal = np.diagonal(numpy_confusion_matrix)
-
-    accuracy = sum(diagonal) / sum(sum(numpy_confusion_matrix))
-
-    recall = []
-    for col_i, column in enumerate(numpy_confusion_matrix.T):
-        recall.append(diagonal[col_i] / sum(column))
-
-    mean_recall = sum(recall) / len(recall)
-
-    worst_by_recall_i = np.argmin(recall)
-    worst_by_recall = countries[worst_by_recall_i]
-    worst_by_recall_confused_most_often_with = countries[np.argsort(numpy_confusion_matrix.T[worst_by_recall_i])[-2]]
-
-    precision = []
-    for row_i, row in enumerate(numpy_confusion_matrix):
-        precision.append(diagonal[row_i] / sum(row))
-
-    mean_precision = sum(precision) / len(precision)
-
-    worst_by_precision_i = np.argmin(recall)
-    worst_by_precision = countries[worst_by_precision_i]
-    worst_by_precision_confused_most_often_with = countries[np.argsort(numpy_confusion_matrix.T[worst_by_precision_i])[-2]]
+    tf_precision = true_positives / col_sums
+    tf_recall = true_positives / row_sums
+    tf_accuracy = true_positives / total_samples
+    tf_f1_score = 2 * (tf_precision * tf_recall) / (tf_precision + tf_recall)
 
     return {
-        'accuracy': accuracy,
-        'mean_recall': mean_recall,
-        'worst_by_recall': worst_by_recall,
-        'worst_by_recall_confused_most_often_with': worst_by_recall_confused_most_often_with,
-        'mean_precision': mean_precision,
-        'worst_by_precision': worst_by_precision,
-        'worst_by_precision_confused_most_often_with': worst_by_precision_confused_most_often_with,
+        'tf_accuracy': tf_accuracy,
+        'tf_precision': tf_precision,
+        'tf_recall': tf_recall,
+        'tf_f1_score': tf_f1_score,
     }
 
 
